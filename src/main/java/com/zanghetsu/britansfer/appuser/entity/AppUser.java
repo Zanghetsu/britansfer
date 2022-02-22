60896c5cad29904ca62b5dd41773ce1337177c7b
Zanghetsu <pickace07@gmail.com>
2022-02-22 15:37:56 +0100

    Adds basic class annotations, and constructor without id

--- src/main/java/com/zanghetsu/britansfer/appuser/entity/AppUser.java
+++ src/main/java/com/zanghetsu/britansfer/appuser/entity/AppUser.java
@@ -1,15 +1,25 @@
 package com.zanghetsu.britansfer.appuser.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.UUID;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
 public class AppUser implements UserDetails {
+
 
     private UUID id;
     private String userName;
@@ -21,7 +31,18 @@ public class AppUser implements UserDetails {
     private UserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
-    
+
+    public AppUser(String userName, String firstName, String lastName, String email, LocalDateTime dateOfBirth, String password, UserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
