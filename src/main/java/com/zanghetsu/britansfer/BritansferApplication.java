ab68a38828527a1371969603a0f21595b7807441
Zanghetsu <pickace07@gmail.com>
2022-02-22 14:16:08 +0100

    Initialized basic application
    dependencies for JPA and psql are commented out for now

---
+++ src/main/java/com/zanghetsu/britansfer/BritansferApplication.java
@@ -0,0 +1,14 @@
+package com.zanghetsu.britansfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritansferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BritansferApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/zanghetsu/britansfer/BritansferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanghetsu.britansfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BritansferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
