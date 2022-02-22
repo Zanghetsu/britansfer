745484757efee00b52f7504c42602f85be079563
Zanghetsu <pickace07@gmail.com>
2022-02-22 16:17:34 +0100

    Implements registration controller

---
+++ src/main/java/com/zanghetsu/britansfer/registration/controller/RegistrationController.java
@@ -0,0 +1,20 @@
+package com.zanghetsu.britansfer.registration.controller;
+
+import com.zanghetsu.britansfer.registration.entity.RegistrationRequest;
+import com.zanghetsu.britansfer.registration.service.RegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+}
