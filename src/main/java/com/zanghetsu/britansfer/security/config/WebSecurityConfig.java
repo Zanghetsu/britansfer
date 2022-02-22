fce7b42252912982bd293e2d696a8a5527df2bf3
Zanghetsu <pickace07@gmail.com>
2022-02-22 16:38:36 +0100

    Adds security package, with security configurations

---
+++ src/main/java/com/zanghetsu/britansfer/security/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.zanghetsu.britansfer.security.config;
+
+import com.zanghetsu.britansfer.appuser.service.AppUserService;
+import com.zanghetsu.britansfer.security.encrypter.PasswordEncrypter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder encoder;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().authorizeRequests()
+                .antMatchers("/api/v*/registration/**")
+                .permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+}
