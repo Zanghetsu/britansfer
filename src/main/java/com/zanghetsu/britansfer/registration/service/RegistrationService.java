c217e96513deb5f6ef2579da82fd2f830688c9b6
Zanghetsu <pickace07@gmail.com>
2022-02-22 16:19:09 +0100

    Implements Registration service

---
+++ src/main/java/com/zanghetsu/britansfer/registration/service/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.zanghetsu.britansfer.registration.service;
+
+import com.zanghetsu.britansfer.registration.entity.RegistrationRequest;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "functioning!";
+    }
+}
