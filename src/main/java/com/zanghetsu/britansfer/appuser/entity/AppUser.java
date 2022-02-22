c19be60964111e58425b0bd29229010c79b1fe0d
Zanghetsu <pickace07@gmail.com>
2022-02-22 15:15:27 +0100

    Initializes User Entity & User Roles (enum)

---
+++ src/main/java/com/zanghetsu/britansfer/appuser/entity/AppUser.java
@@ -0,0 +1,55 @@
+package com.zanghetsu.britansfer.appuser.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class AppUser implements UserDetails {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserRole appUserRole;
+    private Boolean activated;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
