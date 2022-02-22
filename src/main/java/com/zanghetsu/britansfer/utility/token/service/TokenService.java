7ad2b01184bde4521c8cea4618e739b3d5dbfc1e
Zanghetsu <pickace07@gmail.com>
2022-02-22 22:35:31 +0100

    Implements token service

--- src/main/java/com/zanghetsu/britansfer/utility/token/service/TokenService.java
+++ src/main/java/com/zanghetsu/britansfer/utility/token/service/TokenService.java
@@ -1,4 +1,17 @@
 package com.zanghetsu.britansfer.utility.token.service;
 
+import com.zanghetsu.britansfer.utility.token.entity.ConfirmationToken;
+import com.zanghetsu.britansfer.utility.token.repository.TokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class TokenService {
+
+    private final TokenRepository tokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        tokenRepository.save(token);
+    }
 }
