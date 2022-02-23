378a154927ebd5f3255a72401c734045beb0efdd
Zanghetsu <pickace07@gmail.com>
2022-02-23 13:05:14 +0100

    refactors transferRepo

--- src/main/java/com/zanghetsu/britansfer/accounting/repository/AccountingRepository.java
+++ src/main/java/com/zanghetsu/britansfer/accounting/repository/AccountingRepository.java
@@ -2,11 +2,12 @@ package com.zanghetsu.britansfer.accounting.repository;
 
 import com.zanghetsu.britansfer.accounting.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AccountingRepository extends JpaRepository<Transfer, UUID> {
-    Optional<Transfer> findTransferById();
-
+    Optional<Transfer> findTransferById(String transferId);
 }
