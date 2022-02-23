57edec0ffb03022d05ec4a5a9971e94b6df6a870
Zanghetsu <pickace07@gmail.com>
2022-02-23 13:05:58 +0100

    adds account manager refator - custom queries to be established!

--- src/main/java/com/zanghetsu/britansfer/accountmanager/repository/AccountManagerRepo.java
+++ src/main/java/com/zanghetsu/britansfer/accountmanager/repository/AccountManagerRepo.java
@@ -6,20 +6,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 @Repository
 public interface AccountManagerRepo extends JpaRepository<Account,Long> {
-    Optional<Account> findAccountByUserID(Long userId);
+
+    Optional<Account> getAccountByAcountNumber(String accountNumber);
     Optional<Account> findAccountByAccountNumber(String accountNumber);
+    Optional<Account> findAccountByUserID(Long userId);
 
-    @Query("SELECT * FROM accounts WHERE user_id = id")
-    Collection<? extends Account> getAllAccount(Long id);
+    @Query("WHERE a.app_user_id like ?1")
+    List<Account> getAllAccount(Long id);
 
-    @Query("UPDATE account WHERE accountNumber")
+    @Query("UPDATE account a SET a.balance = ?2 WHERE accountNumber = ?1")
     void updateBalance(String accountNumber, BigDecimal amount);
 
-    Optional<Account>
+
+
 }
