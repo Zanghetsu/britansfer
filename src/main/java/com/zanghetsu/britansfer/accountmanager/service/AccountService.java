8132a28b4705e4bae15f028c9ea6fd3ac8a324ea
Zanghetsu <pickace07@gmail.com>
2022-02-23 01:05:02 +0100

    adds basic account manager concept

---
+++ src/main/java/com/zanghetsu/britansfer/accountmanager/service/AccountService.java
@@ -0,0 +1,40 @@
+package com.zanghetsu.britansfer.accountmanager.service;
+
+import com.zanghetsu.britansfer.accountmanager.entity.Account;
+import com.zanghetsu.britansfer.accountmanager.repository.AccountManagerRepo;
+import com.zanghetsu.britansfer.appuser.entity.AppUser;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+public class AccountService {
+
+    private final AccountManagerRepo accMngrRepo;
+
+    public String generateAccount(AppUser appUser){return null;}
+
+    public BigDecimal getBalance(Long id, Account account){
+        return null;
+    }
+
+    public Set<Account> getAccountsForUser(AppUser appUser){
+        return null;
+    }
+
+    private boolean checkBalance(Account account){
+        return false;
+    } // --> currency types??
+
+    public void updateBalance(Account account,BigDecimal amount){return;}  //---> deposit types to be enumerated??
+
+
+
+
+
+
+
+}
