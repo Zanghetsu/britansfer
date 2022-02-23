0d38efdaa849410594d3b2b8b2754d4e5625b8b0
Zanghetsu <pickace07@gmail.com>
2022-02-23 01:05:02 +0100

    adds basic account manager concept

--- src/main/java/com/zanghetsu/britansfer/accountmanager/repository/AccountManagerRepo.java
+++ src/main/java/com/zanghetsu/britansfer/accountmanager/repository/AccountManagerRepo.java
@@ -2,9 +2,11 @@ package com.zanghetsu.britansfer.accountmanager.repository;
 
 import com.zanghetsu.britansfer.accountmanager.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountManagerRepo extends JpaRepository<Account,Long> {
     Optional<Account> findAccountByUserID(Long userId);
 }
